3c2c65a3b20c1aec86dcabe830aa033de39856fc
borelsky <borelsky@gmail.com>
2024-07-04 16:36:02 -0700

    ended the course, went through the 14 chapters, now ready to start developping awesome apps with Next.js

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from 'next';
+
+// handling metadata for Customers only
+export const metadata: Metadata = {
+  title: 'Customers',
+};
 export default function Page() {
   return <p>Customers Page</p>;
 }
